325a329cca8c104172095cb1e0a0118086e755ae
Matt Gibb <powermoveguru@googlemail.com>
2009-11-16 17:26:56 +0000

    itkTileImageFilter builds 3D volume out of 2D slices, aligning them at the bottom left corner.

--- itk/Register.cxx
+++ itk/Register.cxx
@@ -3,6 +3,10 @@
 #include "itkImageFileWriter.h"
 #include "itkRegularExpressionSeriesFileNames.h"
 #include "itkTileImageFilter.h"
+#include "itkResampleImageFilter.h"
+// #include "itkTranslationTransform.h"
+#include "itkChangeInformationImageFilter.h"
+#include "itkNearestNeighborInterpolateImageFunction.h"
 
 using namespace std;
 
@@ -50,30 +54,12 @@ int main (int argc, char const *argv[])
 
 
   // set up input and output types
-  typedef unsigned char PixelType;
+  typedef unsigned short PixelType;
   const unsigned int InputDimension  = 2;
   const unsigned int OutputDimension = 3;
 
   typedef itk::Image< PixelType, InputDimension  > InputImageType;
   typedef itk::Image< PixelType, OutputDimension > OutputImageType;
-
-
-  // set up vector of input images
-  typedef itk::ImageFileReader< InputImageType > ReaderType;
-  ReaderType::Pointer reader = ReaderType::New();
-
-	typedef vector< InputImageType::Pointer > InputVectorType;
-  InputVectorType slices;
-  InputVectorType::iterator inputIter;
-  
-	for(fileIter=fileNames.begin(); fileIter!=fileNames.end(); fileIter++)
-	{
-		reader->SetFileName( *fileIter );
-		reader->Update();
-		cout << *fileIter << endl;
-		slices.push_back( reader->GetOutput() );
-		// slices.back()->DisconnectPipeline();
-	}
 
 
   // set up tile image filter
@@ -82,43 +68,87 @@ int main (int argc, char const *argv[])
 	
 	TileFilterType::LayoutArrayType layout;
 	layout[0] = 1;
-	layout[1] = 1;
-	layout[2] = 0;
+	layout[1] = 0;
+	layout[2] = 1;
 	tileFilter->SetLayout( layout );
+		
+	
+  // set up vector of input images and link to tile filter
+	typedef vector< InputImageType::Pointer > InputVectorType;
+  InputVectorType slices;
+  InputVectorType::iterator inputIter;
+  
+  typedef itk::ImageFileReader< InputImageType > ReaderType;
+	typedef itk::ChangeInformationImageFilter<InputImageType> CenterType;
+
+	// typedef itk::TranslationTransform< double, Dimension >  TransformType;
 	
 	
-  // write results to file
+	for(fileIter=fileNames.begin(); fileIter!=fileNames.end(); fileIter++)
+	{
+		ReaderType::Pointer reader = ReaderType::New();
+		reader->SetFileName( *fileIter );
+		CenterType::Pointer centerer = CenterType::New();
+		centerer->CenterImageOn();
+		centerer->SetInput( reader->GetOutput() );
+		centerer->Update();
+		cout << "centerer->GetOutputOrigin() = " << centerer->GetOutputOrigin() << endl;
+		cout << "centerer->GetOutputOffset() = " << *(centerer->GetOutputOffset()) << endl;
+		slices.push_back( centerer->GetOutput() );
+		cout << "slices.back()->GetOrigin() = " << slices.back()->GetOrigin() << endl;	
+		tileFilter->PushBackInput( slices.back() );
+		// slices.back()->DisconnectPipeline();
+	}
+  
+
+  // Scale results in the z-direction
+  typedef itk::ChangeInformationImageFilter<OutputImageType> ZScaleType;
+	ZScaleType::Pointer zScaler = ZScaleType::New();
+	
+	zScaler->SetInput( tileFilter->GetOutput() );
+	
+	zScaler->ChangeSpacingOn();
+	ZScaleType::SpacingType spacings;
+	spacings[0] = 128;
+	spacings[1] = 128;
+	spacings[2] = 128;
+	zScaler->SetOutputSpacing( spacings );
+	cout << "zScaler->GetOutputSpacing(): " << zScaler->GetOutputSpacing() << endl;
+
+
   typedef itk::ImageFileWriter< OutputImageType > WriterType;
   WriterType::Pointer writer = WriterType::New();
 
-	writer->SetInput( tileFilter->GetOutput() );
+	writer->SetInput( zScaler->GetOutput() );
   
   std::string outputFilename = argv[4];
   writer->SetFileName( outputFilename );
 
-  //TEMPORARY
-	typedef itk::ImageFileWriter< InputImageType > WriterType2;
-	WriterType2::Pointer writer2 = WriterType2::New();
-	
-  for(unsigned int i=0; i<slices.size(); i++)
-	{
-		writer2->SetFileName( string("testdir/") + fileNames[i] );
-		writer2->SetInput( slices[i] );
-		writer2->Update();
-	}
-	
-  //TEMPORARY
-
-
   try 
     { 
-    // writer->Update(); 
+    writer->Update();
     } 
   catch( itk::ExceptionObject & err ) 
     { 
     std::cerr << "ExceptionObject caught !" << std::endl; 
     std::cerr << err << std::endl; 
     return EXIT_FAILURE;
-    } 
+    }
+
+
+  //TEMPORARY
+	// typedef itk::ImageFileWriter< InputImageType > WriterType2;
+	// WriterType2::Pointer writer2 = WriterType2::New();
+	// 
+	//   for(unsigned int i=0; i<slices.size(); i++)
+	// {
+	// 	writer2->SetFileName( string("testdir/") + fileNames[i] );
+	// 	writer2->SetInput( slices[i] );
+	// 	writer2->Update();
+	// }
+	// 
+  //TEMPORARY
+
+
   return EXIT_SUCCESS;
 }
