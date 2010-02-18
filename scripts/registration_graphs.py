8676cae2ccd4e15b479a56025f5da2293d407cfb
Matt Gibb <powermoveguru@googlemail.com>
2010-02-18 11:44:59 +0000

    registration_graphs.py successfully plots iteration parameters.

--- scripts/registration_graphs.py
+++ scripts/registration_graphs.py
@@ -1,29 +1,87 @@
 #!/usr/bin/env python
 import os, sys
 import matplotlib.pyplot as plt
-import numpy.random as random
-import re
+from numpy import * 
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.axes import subplot_class_factory
+
+from IPython.Shell import IPShellEmbed
+ipshell = IPShellEmbed()
+
+# extract command line arguments
+params_file, output_dir = sys.argv[1:3]
 
 # read registration output
-f = open(sys.argv[1])
+f = open(params_file)
 lines = f.readlines()
-print lines[0]
 
-regexp = '([0-9]+) = '
-match = re.search(regexp, lines[0])
-print ''.join(['match.group(0) = \'', match.group(0), '\''])
-print ''.join(['match.group(1) = \'', match.group(1), '\''])
+# clean up registration output
+for line_index, line in enumerate(lines):
+	line = line.rsplit()
+	cleaned_line = []
+	# 'line[:]' instead of 'line' to make a temporary copy
+	for word_index, word in enumerate(line[:]):
+		word = word.lstrip('[:=')
+		word = word.rstrip(':=,]')
+		if not len(word) == 0:
+			cleaned_line.append(float(word))
+	lines[line_index] = cleaned_line
 
-# files = []
-# fig = plt.figure(figsize=(5,5))
-# ax = fig.add_subplot(111)
-# for i in range(50):
-# 	ax.cla()
-# 	ax.imshow(random.rand(5,5), interpolation='nearest')
-# 	fname = '_tmp%03d.png'%i
-# 	print 'Saving frame', fname
-# 	fig.savefig(fname)
-# 	files.append(fname)
-# 	
-# print 'Making movie animation.mpg - this may take a while'
-# os.system("mencoder 'mf://_tmp*.png' -mf type=png:fps=10 -ovc lavc -lavcopts vcodec=wmv2 -oac copy -o animation.avi")
+# extract data from lines
+lines = asarray(lines)
+iteration = lines[:,0]
+metric_value = lines[:,1]
+versor_params = lines[:,2:5]
+translation_params = lines[:,5:8]
+
+# generate movie of parameters
+fig = plt.figure(figsize=(15,5))
+Subplot3D = subplot_class_factory(Axes3D)
+
+versor_ax      = Subplot3D(fig, 131)
+translation_ax = Subplot3D(fig, 132)
+metric_ax      = plt.subplot(133)
+
+versor_ax.plot(versor_params[:,0],versor_params[:,1],versor_params[:,2])
+translation_ax.plot(translation_params[:,0],translation_params[:,1],translation_params[:,2])
+metric_ax.plot(metric_value)
+
+versor_lims = versor_ax.get_w_lims()
+translation_lims = translation_ax.get_w_lims()
+metric_lims = metric_ax.get_xlim() + metric_ax.get_ylim()
+
+# metric_lims = metric_ax
+
+for i in range(1,len(iteration)):
+	versor_ax.cla()
+	translation_ax.cla()
+	metric_ax.cla()
+	
+	versor_ax.plot(versor_params[:i,0],versor_params[:i,1],versor_params[:i,2])
+	translation_ax.plot(translation_params[:i,0],translation_params[:i,1],translation_params[:i,2])
+	metric_ax.plot(metric_value[:i])
+	
+	versor_ax.set_title('Versor Parameters')
+	translation_ax.set_title('Translation Parameters')
+	metric_ax.set_title('Metric Value')
+	
+	versor_ax.set_xlim3d(versor_lims[0:2])
+	versor_ax.set_ylim3d(versor_lims[2:4])
+	versor_ax.set_zlim3d(versor_lims[4:6])
+	translation_ax.set_xlim3d(translation_lims[0:2])
+	translation_ax.set_ylim3d(translation_lims[2:4])
+	translation_ax.set_zlim3d(translation_lims[4:6])
+	metric_ax.set_xlim(metric_lims[0:2])
+	metric_ax.set_ylim(metric_lims[2:4])
+	
+	versor_ax.azim +=1
+	translation_ax.azim +=1
+	
+	fname = '%s/_tmp%03d.png' % (output_dir, i)
+	print 'Saving frame', fname
+	fig.savefig(fname)
+  
+print 'Making movie animation.mpg - this may take a while'
+os.system("cd %s && mencoder 'mf://_tmp*.png' -mf type=png:fps=10 -ovc lavc -lavcopts vcodec=mpeg4 -oac copy -o animation.avi" % output_dir )
+print 'Removing png files...'
+os.system('rm %s/*.png' % output_dir )
