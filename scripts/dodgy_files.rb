e384afad7459f1bd72f6898623a00b5e0a5a084f
Matt Gibb <powermoveguru@googlemail.com>
2010-05-07 19:54:13 +0100

    Refactored dodgy_files.rb so it can be included from irb.

--- scripts/dodgy_files.rb
+++ scripts/dodgy_files.rb
@@ -4,14 +4,18 @@ IMAGES_DIR = '../../images'
 DOWNSAMPLES_DIR = IMAGES_DIR + '/downsamples'
 DODGY_DIR = IMAGES_DIR + '/dodgy_downsamples'
 
+def print_differences(files)
+  files.each {|f| puts "differences for #{f}:", `diff #{DODGY_DIR}/#{f} #{DOWNSAMPLES_DIR}/#{f}` }
+end
+
 # generate lists of filenames
 downsamples = dodgy = nil
 downsamples = Dir.chdir(DOWNSAMPLES_DIR) { downsamples = Dir['*'] }
 dodgy       = Dir.chdir(DODGY_DIR)       { dodgy       = Dir['*'] }
 
 # find common files, sorted by when they were downsampled
-both = downsamples & dodgy
-both = both.sort_by {|f| File.mtime "#{DOWNSAMPLES_DIR}/#{f}"}
+@both = downsamples & dodgy
+@both = @both.sort_by {|f| File.mtime "#{DOWNSAMPLES_DIR}/#{f}"}
 
 # print out differences between dodgy files and newly downsampled files
-both.each {|f| puts "differences for #{f}:", `diff #{DODGY_DIR}/#{f} #{DOWNSAMPLES_DIR}/#{f}` }
+print_differences(@both)
