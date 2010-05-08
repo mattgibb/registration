02ca2765e0abca00a214dfbf16b952d3db388844
Matt Gibb <powermoveguru@googlemail.com>
2010-05-08 01:40:02 +0100

    Further refactoring of dodgy_files.rb.

--- scripts/dodgy_files.rb
+++ scripts/dodgy_files.rb
@@ -4,34 +4,35 @@ IMAGES_DIR = '../../images'
 DOWNSAMPLES_DIR = IMAGES_DIR + '/downsamples'
 DODGY_DIR = IMAGES_DIR + '/dodgy_downsamples'
 
-def print_differences(files)
-  files.each do |f|
-    puts "differences for #{f}:", `diff #{DODGY_DIR}/#{f} #{DOWNSAMPLES_DIR}/#{f}`
-  end
-end
-
-def remove_dodgy_files(files)
-  files.each do |f|
-    `rm #{DOWNSAMPLES_DIR}/#{f}` if `diff #{DODGY_DIR}/#{f} #{DOWNSAMPLES_DIR}/#{f}`.empty?
-  end
-end
-
 def common_files(params)
   # generate lists of filenames
   downsamples = Dir.chdir(DOWNSAMPLES_DIR) { Dir['*'] }
   dodgy       = Dir.chdir(DODGY_DIR)       { Dir['*'] }
   
-  # find common files, sorted by when they were downsampled
+  # find common files
   @both = downsamples & dodgy
   
+  # sort files
   case params[:sorted_by]
-  when :name  then @both.sort
   when :mtime then @both.sort_by {|f| File.mtime "#{DOWNSAMPLES_DIR}/#{f}"}
   when :atime then @both.sort_by {|f| File.atime "#{DOWNSAMPLES_DIR}/#{f}"}
   when :size  then @both.sort_by {|f| File.size  "#{DOWNSAMPLES_DIR}/#{f}"}
-  end
-  
+  when :name  then @both.sort
+  end  
 end
 
-# print out differences between dodgy files and newly downsampled files
-print_differences(common_files sorted_by: :mtime)
+def dodgy_files(params)
+  common_files(params).select do |f|
+    `diff #{DODGY_DIR}/#{f} #{DOWNSAMPLES_DIR}/#{f}`.empty?
+  end
+end
+
+def candidate_files(params)
+  common_files(params) - dodgy_files(params)
+end
+
+def remove_dodgy_files
+  dodgy_files().each do |f|
+    `rm #{DODGY_DIR}/#{f}`
+  end
+end
