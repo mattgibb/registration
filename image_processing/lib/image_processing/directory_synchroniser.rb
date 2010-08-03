a427c10a364e3a200ccfa74039ff20ee754a429b
Matt Gibb <powermoveguru@gmail.com>
2010-08-03 11:01:34 +0100

    Added directory synchroniser to image processing suite.

---
+++ image_processing/lib/image_processing/directory_synchroniser.rb
@@ -0,0 +1,22 @@
+require 'image_processing/ftp_adaptor'
+
+module ImageProcessing
+  class DirectorySynchroniser
+    attr_accessor :server
+    
+    def initialize(host, remote_dir, local_dir)
+      @host, @remote_dir, @local_dir = host, remote_dir, local_dir
+      @ftp = FtpAdaptor.new(@host, 'mef', 'meflab')
+    end
+    
+    def download
+      remote_files = @ftp.list(@remote_dir).map {|f| File.basename f }
+      local_files = Dir.chdir(@local_dir) { Dir['*'] }
+      files_remaining = remote_files - local_files
+      
+      files_remaining.each {|file| @ftp.download_file(@remote_dir + file, @local_dir + file) }
+      
+      puts "All available files have been uploaded!"
+    end
+  end
+end
