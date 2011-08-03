cf2a2d4a92f7d55fa0d552e75aff59728706db57
Matt Gibb <powermoveguru@gmail.com>
2011-08-03 15:50:12 +0100

    Removed imageDimensions() singleton method.

--- itk_source/lib/Parameters.cxx
+++ itk_source/lib/Parameters.cxx
@@ -7,26 +7,6 @@
 #include "Dirs.hpp"
 
 
-YAML::Node& imageDimensions()
-{
-  // only initialize statics when registrationParameters is first called
-  static YAML::Node imageDimensions;
-  static bool initialized = false;
-  
-  // initialize registrationParameters if hasn't been already
-  if(!initialized)
-  {
-    ifstream config_filestream( (Dirs::ConfigDir() + "image_dimensions.yml").c_str() );
-    YAML::Parser parser(config_filestream);
-    parser.GetNextDocument(imageDimensions);
-    initialized = true;
-  }
-  
-  // return reference to singleton
-  return imageDimensions;
-}
-
-
 YAML::Node& registrationParameters()
 {
   // only initialize statics when registrationParameters is first called
