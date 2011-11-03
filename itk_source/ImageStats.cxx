42675b882bde687755345cb67bd290369b198133
Matt Gibb <matt@mattgibb.com>
2011-11-03 16:08:27 +0000

    Refactored image stats out into a function.

--- itk_source/ImageStats.cxx
+++ itk_source/ImageStats.cxx
@@ -1,5 +1,7 @@
 #include "itkImageFileReader.h"
-#include "itkStatisticsImageFilter.h"
+
+// my files
+#include "ImageStats.hpp"
 
 using namespace std;
 
@@ -14,38 +16,16 @@ int main( int argc, char ** argv )
 
   typedef unsigned char      PixelType;
   const   unsigned int        Dimension = 2;
-  typedef itk::Image< PixelType, Dimension >    ImageType;
-  typedef itk::ImageFileReader< ImageType >  ReaderType;
-  typedef itk::StatisticsImageFilter< ImageType > StatsType;
+  typedef itk::Image< PixelType, Dimension > ImageType;
+  typedef itk::ImageFileReader< ImageType > ReaderType;
 
   ReaderType::Pointer reader = ReaderType::New();
-  StatsType::Pointer  stats  = StatsType::New();
 
   const char * inputFilename  = argv[1];
 
-  reader->SetFileName( inputFilename  );
-
-  stats->SetInput( reader->GetOutput() );
+  reader->SetFileName( inputFilename );
   
-  try 
-    { 
-    stats->Update(); 
-    } 
-  catch( itk::ExceptionObject & err ) 
-    { 
-    std::cerr << "ExceptionObject caught !" << std::endl; 
-    std::cerr << err << std::endl; 
-    return EXIT_FAILURE;
-    } 
-
-  // Show statistics
-  cerr << "Minimum:\n" << stats->GetMinimum() + 0 << endl << endl;
-  cerr << "Maximum:\n" << stats->GetMaximum() + 0 << endl << endl;
-  cerr << "Mean:\n" << stats->GetMean() << endl << endl;
-  cerr << "Sigma:\n" << stats->GetSigma() << endl << endl;
-  cerr << "Variance:\n" << stats->GetVariance() << endl << endl;
-  cerr << "Sum:\n" << stats->GetSum() << endl << endl;
-  cerr << "Image:\n" << stats->GetOutput() << endl << endl;
-
+  printImageStats< ImageType >( reader->GetOutput() );
+  
   return EXIT_SUCCESS;
 }
---
+++ itk_source/lib/ImageStats.hpp
@@ -0,0 +1,34 @@
+#ifndef IMAGE_STATS_HPP_
+#define IMAGE_STATS_HPP_
+
+#include "itkStatisticsImageFilter.h"
+
+using namespace std;
+
+template<class TImage>
+void printImageStats(typename TImage::Pointer image)
+{
+  typedef itk::StatisticsImageFilter< TImage > StatsType;
+  typename StatsType::Pointer stats = StatsType::New();
+  stats->SetInput( image );
+  try 
+  { stats->Update(); } 
+  catch( itk::ExceptionObject & err ) 
+  { 
+    std::cerr << "ExceptionObject caught !" << std::endl; 
+    std::cerr << err << std::endl; 
+    exit(EXIT_FAILURE);
+  }
+  
+  // Show statistics
+  cerr << "Minimum:\n" << stats->GetMinimum() + 0 << endl << endl;
+  cerr << "Maximum:\n" << stats->GetMaximum() + 0 << endl << endl;
+  cerr << "Mean:\n" << stats->GetMean() << endl << endl;
+  cerr << "Sigma:\n" << stats->GetSigma() << endl << endl;
+  cerr << "Variance:\n" << stats->GetVariance() << endl << endl;
+  cerr << "Sum:\n" << stats->GetSum() << endl << endl;
+  cerr << "Image:\n" << stats->GetOutput() << endl << endl;
+  
+}
+
+#endif
