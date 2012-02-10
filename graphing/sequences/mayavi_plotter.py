e84462aa10c76ee443c0d48d102d3ff9fcc95f77
Matt Gibb <matt@mattgibb.com>
2012-02-10 12:54:44 +0000

    Removed graphing Python scripts.

--- graphing/sequences/mayavi_plotter.py
+++
@@ -1,5 +0,0 @@
-from sequences.plotter_base import PlotterBase
-
-class MayaviPlotter(PlotterBase):
-    def plot(self):
-        pass
--- graphing/sequences/plotter_base.py
+++
@@ -1,5 +0,0 @@
-class PlotterBase:
-    def __init__(self, output_dir, *sources):
-        self.sources = sources
-        self.output_dir = output_dir
-    
