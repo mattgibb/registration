03a458c5f6a817d13dc24357bd13c392208d6453
Matt Gibb <matt@mattgibb.com>
2012-02-21 14:58:40 +0000

    Registration of adjacent slice pairs works.

--- itk_source/RegisterHiResPairs.cxx
+++ itk_source/RegisterHiResPairs.cxx
@@ -1,109 +1,150 @@
-// Register 2 HiRes images to each other,
+// Register adjacent HiRes images to each other,
 // initialised by loaded transforms from previous registration
 
+#include "boost/filesystem.hpp"
+#include "boost/program_options.hpp"
+
+// my files
+#include "Stack.hpp"
+#include "RegistrationBuilder.hpp"
+#include "StackIOHelpers.hpp"
+#include "IOHelpers.hpp"
+#include "StackTransforms.hpp"
+#include "Dirs.hpp"
+#include "OptimizerConfig.hpp"
+#include "ScaleImages.hpp"
+
 using namespace boost::filesystem;
-  
+namespace po = boost::program_options;
+using namespace boost;
+
 // function declarations
 po::variables_map parse_arguments(int argc, char *argv[]);
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char *argv[]) {
   po::variables_map vm = parse_arguments(argc, argv);
-  
-  // Verify the number of parameters in the command line
-  checkUsage(argc, argv);
   
   // Process command line arguments
   Dirs::SetDataSet(argv[1]);
   Dirs::SetOutputDirName(argv[2]);
-  string fixedBasename( vm["fixedBasename"].as<string>() );
-  string movingBasename( vm["movingBasename"].as<string>() );
-  vector< string > fixedPath( 1, Dirs::SliceDir() + fixedBasename  + ".bmp");
-  vector< string > movingPath(1, Dirs::SliceDir() + movingBasename + ".bmp");
-  string roi = vm.count("roi") ? vm["roi"].as<string>() : "whole_heart";
+  string roi = vm.count("roi") ? vm["roi"].as<string>() : "ROI";
   
-  // initialise stack objects with correct spacings, sizes etc
+  // for globally available registrationParameters(),
+  // used in e.g. OptimizerConfig
+  Dirs::SetParamsFile( Dirs::ConfigDir() + "HiRes_pair_parameters.yml" );
+  
+  // construct basenames and paths
+  vector< string > basenames = getBasenames(Dirs::ImageList());
+  vector< string > paths = constructPaths(Dirs::SliceDir(), basenames, ".bmp");
+  
+  // initialise stack with correct spacings, sizes, basenames etc
   typedef Stack< float, itk::ResampleImageFilter, itk::LinearInterpolateImageFunction > StackType;
-  StackType::SliceVectorType FixedOriginalImage = readImages< StackType::SliceType >(fixedPath);
-  StackType::SliceVectorType MovingOriginalImage = readImages< StackType::SliceType >(movingPath);
-  boost::shared_ptr< StackType > FixedOriginalStack = InitializeHiResStack<StackType>(FixedOriginalImage, roi);
-  boost::shared_ptr< StackType > MovingOriginalStack = InitializeHiResStack<StackType>(MovingOriginalImage, roi);
-  FixedOriginalStack->SetBasenames(vector< string >(1, fixedBasename));
-  MovingOriginalStack->SetBasenames(vector< string >(1, movingBasename));
+  StackType::SliceVectorType originalImages = readImages< StackType::SliceType >(paths);
+  scaleImages< StackType::SliceType >(originalImages, getSpacings<2>("HiRes"));
+  shared_ptr< StackType > originalStack = make_shared< StackType >(originalImages, getSpacings<3>("LoRes"), getSize(roi));
+  originalStack->SetBasenames(basenames);
   
-  Load(*FixedOriginalStack,  Dirs::HiResTransformsDir(), fixedBasename);
-  Load(*MovingOriginalStack, Dirs::HiResTransformsDir(), movingBasename);
+  Load(*originalStack, Dirs::HiResTransformsDir() + "CenteredAffineTransform/");
   
   // move stack origins to ROI
   itk::Vector< double, 2 > translation = StackTransforms::GetLoResTranslation(roi) - StackTransforms::GetLoResTranslation("whole_heart");
-  StackTransforms::Translate(*FixedOriginalStack, translation);
-  StackTransforms::Translate(*MovingOriginalStack, translation);
+  StackTransforms::Translate(*originalStack, translation);
   
   // generate images
-  FixedOriginalStack->updateVolumes();
-  MovingOriginalStack->updateVolumes();
+  originalStack->updateVolumes();
   
-  // Make stacks out of resampled slices
-  StackType::SliceVectorType FixedImage, MovingImage;
-  FixedImage.push_back(FixedOriginalStack->GetResampledSlice(0));
-  MovingImage.push_back(MovingOriginalStack->GetResampledSlice(0));
-  boost::shared_ptr< StackType > FixedStack = InitializeHiResStack<StackType>(FixedImage, roi);
-  boost::shared_ptr< StackType > MovingStack = InitializeHiResStack<StackType>(MovingImage, roi);
-  
+  // construct 2 stacks from resampled images: 1:n-1 and 2:n
+  StackType::SliceVectorType resampledImages = originalStack->GetResampledSlices();
+  StackType::SliceVectorType fixedImages(resampledImages.begin(), --resampledImages.end());
+  StackType::SliceVectorType movingImages(++resampledImages.begin(), resampledImages.end());
+  StackType::MaskVectorType2D resampledMasks = originalStack->GetResampled2DMasks();
+  StackType::MaskVectorType2D fixedMasks(resampledMasks.begin(), --resampledMasks.end());
+  StackType::MaskVectorType2D movingMasks(++resampledMasks.begin(), resampledMasks.end());
+  shared_ptr< StackType > fixedStack  = make_shared< StackType >(fixedImages,  fixedMasks,  getSpacings<3>("LoRes"), getSize(roi));
+  shared_ptr< StackType > movingStack = make_shared< StackType >(movingImages, movingMasks, getSpacings<3>("LoRes"), getSize(roi));
+
   // Build registration
-  boost::shared_ptr<YAML::Node> pParameters = config("HiRes_pair_parameters.yml");
   typedef RegistrationBuilder< StackType > RegistrationBuilderType;
-  RegistrationBuilderType registrationBuilder(pParameters);
+  RegistrationBuilderType registrationBuilder;
   RegistrationBuilderType::RegistrationType::Pointer registration = registrationBuilder.GetRegistration();
   
-  // SET AFFINE? TRANSFORM FOR MOVING STACK
+  // Initialize both stacks with CenteredRigid2DTransform
+  StackTransforms::InitializeToCommonCentre(*fixedStack);
+  StackTransforms::InitializeToCommonCentre(*movingStack);
   
   // Configure moving centre and optimiser scales
-  StackTransforms::SetMovingStackCenterWithFixedStack( *FixedStack, *MovingStack );
+  StackTransforms::SetMovingStackCenterWithFixedStack( *fixedStack, *movingStack );
+  
+  // Convert moving CenteredRigid2DTransform to AffineTransform
+  typedef itk::CenteredAffineTransform< double, 2 > AffineTransformType;
+  StackTransforms::InitializeFromCurrentTransforms< StackType, AffineTransformType >(*movingStack);
+  
   OptimizerConfig::SetOptimizerScalesForCenteredAffineTransform( registration->GetOptimizer() );
   
-  // Perform registration
-  registration->SetFixedImage( FixedOriginalStack->GetResampledSlice(0) );
-  registration->SetMovingImage( MovingOriginalStack->GetResampledSlice(0) );
-  registration->GetMetric()->SetFixedImageMask( FixedOriginalStack->GetResampled2DMask(0) );
-  registration->GetMetric()->SetMovingImageMask( MovingOriginalStack->GetResampled2DMask(0) );
-  registration->SetTransform( MovingStack->GetTransform(0) );
-  registration->SetInitialTransformParameters( MovingStack->GetTransform(0)->GetParameters() );
+  // TEMP
+  fixedStack->updateVolumes();
+  movingStack->updateVolumes();
+  string outputDir = Dirs::ResultsDir() + "HiResPairs/";
+  create_directory( outputDir );
+  writeImage< StackType::VolumeType >( movingStack->GetVolume(), outputDir + "moving_before.mha");
+  writeImage< StackType::MaskVolumeType >( originalStack->Get3DMask()->GetImage(), outputDir + "original_mask.mha");
+  // TEMP
   
-  try { registration->Update(); }
-  catch( itk::ExceptionObject & err ) 
-  { 
-    std::cerr << "ExceptionObject caught !" << std::endl;
-    std::cerr << err << std::endl;
-    return EXIT_FAILURE;
+  // Get number of slices for registration and transform basenames
+  unsigned int number_of_slices = fixedStack->GetSize();
+  
+  // Perform registration
+  for(unsigned int slice_number=0; slice_number < number_of_slices; ++slice_number)
+  {
+    cout << "Registering slices " << basenames[slice_number] <<
+      " and " << basenames[slice_number + 1] << "..." << endl;
+    registration->SetFixedImage( fixedStack->GetOriginalImage(slice_number) );
+    registration->SetMovingImage( movingStack->GetOriginalImage(slice_number) );
+    // registration->GetMetric()->SetFixedImageMask( fixedStack->GetOriginal2DMask(slice_number) );
+    // registration->GetMetric()->SetMovingImageMask( movingStack->GetOriginal2DMask(slice_number) );
+    registration->SetTransform( movingStack->GetTransform(slice_number) );
+    registration->SetInitialTransformParameters( movingStack->GetTransform(slice_number)->GetParameters() );
+    
+    try { registration->Update(); }
+    catch( itk::ExceptionObject & err )
+    { 
+      std::cerr << "ExceptionObject caught !" << std::endl;
+      std::cerr << err << std::endl;
+      return EXIT_FAILURE;
+    }
   }
   
-  // save transform
-  string transformFile = Dirs::HiResPairTransformsDir() + fixedBasename + "_" + movingBasename;
-  writeTransform(MovingStack->GetTransform(0), transformFile);
+  // Save transforms
+  vector< string > transformBasenames;
+  for(unsigned int slice_number=0; slice_number < number_of_slices; ++slice_number)
+  {
+    // construct basenames e.g. "0001_0002"
+    transformBasenames.push_back(basenames[slice_number] + "_" + basenames[slice_number + 1]);
+  }
   
-  // Write bmps
-  using namespace boost::filesystem;
-  create_directory( Dirs::SlicePairDir() );
+  movingStack->SetBasenames(transformBasenames);
+  create_directory(Dirs::HiResPairTransformsDir());
+  Save(*movingStack, Dirs::HiResPairTransformsDir());
   
-  writeImage< StackType::VolumeType >( FixedOriginalStack->GetVolume(), (path( Dirs::SlicePairDir() ) / "LoRes.mha").string());
-  writeImage< StackType::VolumeType >( MovingOriginalStack->GetVolume(), (path( Dirs::SlicePairDir() ) / "HiRes.mha").string());
+  // TEMP
+  string tempDir = Dirs::HiResPairTransformsDir() + "/fixed";
+  create_directory(tempDir);
+  fixedStack->SetBasenames(transformBasenames);
+  Save(*fixedStack, tempDir);
+  // TEMP
   
-  
-  // create_directory(LoResTransformsDir);
-  // create_directory(HiResTransformsDir);
-  // Save(*FixedOriginalStack, LoResTransformsDir, fixedBasename);
-  // Save(*MovingOriginalStack, HiResTransformsDir, movingBasename);
+  // Write images
+  fixedStack->updateVolumes();
+  movingStack->updateVolumes();
+  // string outputDir = Dirs::ResultsDir() + "HiResPairs/";
+  // create_directory( outputDir );
+  writeImage< StackType::VolumeType >( originalStack->GetVolume(), outputDir + "original.mha");
+  writeImage< StackType::VolumeType >( fixedStack->GetVolume(), outputDir + "fixed.mha");
+  writeImage< StackType::VolumeType >( movingStack->GetVolume(), outputDir + "moving_after.mha");
+  writeImage< StackType::MaskVolumeType >( fixedStack->Get3DMask()->GetImage(), outputDir + "fixed_mask.mha");
+  writeImage< StackType::MaskVolumeType >( movingStack->Get3DMask()->GetImage(), outputDir + "moving_mask.mha");
   
   return EXIT_SUCCESS;
-}
-
-void checkUsage(int argc, char const *argv[]) {
-  if( argc < 5 )
-  {
-    cerr << "\nUsage: " << endl;
-    cerr << argv[0] << " dataSet outputDir fixedSlice movingSlice (roi)\n\n";
-    exit(EXIT_FAILURE);
-  }
 }
 
 po::variables_map parse_arguments(int argc, char *argv[])
@@ -114,17 +155,14 @@ po::variables_map parse_arguments(int argc, char *argv[])
       ("help,h", "produce help message")
       ("dataSet", po::value<string>(), "which rat to use")
       ("outputDir", po::value<string>(), "directory to place results")
-      ("fixedBasename", po::value<string>(), "basename of fixed slice")
-      ("movingBasename", po::value<string>(), "basename of moving slice")
-      ("outputTransform", po::value<string>(), "file name of the w ")
-      ("roi", po::value<string>(), "region of interest")
+      ("roi", po::value<string>(), "region of interest e.g. papillary_insertion")
+      ("fixedBasename", po::value<string>(), "basename of fixed slice e.g. 0196")
+      ("movingBasename", po::value<string>(), "basename of moving slice e.g. 0197")
   ;
   
   po::positional_options_description p;
   p.add("dataSet", 1)
-  .add("outputDir", 1)
-  .add("fixedBasename", 1)
-  .add("movingBasename", 1)
+   .add("outputDir", 1)
    .add("roi", 1);
      
   // parse command line
@@ -149,20 +187,16 @@ po::variables_map parse_arguments(int argc, char *argv[])
   if(vm.count("help") ||
     !vm.count("dataSet") ||
     !vm.count("outputDir") ||
-    !vm.count("fixedBasename") ||
-    !vm.count("movingBasename") ||
-    !vm.count("outputTransform") )
+    (vm.count("fixedBasename") != vm.count("movingBasename")) )
   {
     cerr << "Usage: "
       << argv[0]
       << " [--dataSet=]RatX [--outputDir=]my_dir"
-      << " [--fixedBasename=]0196 [--movingBasename=]0197"
-      << " [[--roi=]papillary_insertion] [Options]"
+      << " [Options]"
       << endl << endl;
     cerr << opts << "\n";
     exit(EXIT_FAILURE);
   }
-    
+  
   return vm;
 }
-
