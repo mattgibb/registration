7230c2da6ecacb374a9c35e0012e1b487887b63d
Matt Gibb <matt@mattgibb.com>
2012-02-28 15:35:30 +0000

    Plot delta values.

--- graphing/metric_values.py
+++ graphing/metric_values.py
@@ -13,8 +13,9 @@ affine_dir     = join(metric_values_dir, "CenteredAffineTransform")
 # raise AssertionError if contents of 3 tranform dirs are not the same
 # i.e. contain the same slices
 assert listdir(rigid_dir) == listdir(similarity_dir) == listdir(affine_dir)
+basenames = listdir(rigid_dir)
 
-values = StackMetricValues(rigid_dir).values
+metric_values = StackMetricValues(rigid_dir)
 
 # plot 3d line
 import matplotlib as mpl
@@ -24,11 +25,15 @@ import matplotlib.pyplot as plt
 
 mpl.rcParams['legend.fontsize'] = 10
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-for i, slice in enumerate(values):
-    ax.plot(range(len(slice)), [i] * len(slice), slice, label='boobies!')
-
-ax.legend()
-
-plt.show()
+def plot_3d_lines(values):
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    for i, slice in enumerate(values):
+        x = range(len(slice))
+        y = [i] * len(slice)
+        ax.plot(x, y, slice, label=basenames[i])
+    ax.legend()
+    plt.show()
+    
+plot_3d_lines(metric_values.values())
+plot_3d_lines(metric_values.delta_values())
--- graphing/stack_metric_values.py
+++ graphing/stack_metric_values.py
@@ -1,7 +1,15 @@
 from os.path import *
 from os import listdir
-from numpy import genfromtxt
+from numpy import genfromtxt, array
 
 class StackMetricValues:
     def __init__(self, transform_dir):
-        self.values = [genfromtxt(join(transform_dir, file)) for file in listdir(transform_dir)]
+        value_list = [genfromtxt(join(transform_dir, file)) for file in listdir(transform_dir)]
+        self._values = array(value_list)
+    
+    def values(self):
+        return self._values
+    
+    def delta_values(self):
+        # subtract the previous value from each value
+        return self._values[:,1:] - self._values[:,:-1]
