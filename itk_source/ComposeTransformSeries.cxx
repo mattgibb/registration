2c56da0d3b1bdb216709216eaf21e504d6948811
Matt Gibb <matt@mattgibb.com>
2012-04-11 14:42:56 +0100

    Compose diffusion transforms from originals and adjustments

--- itk_source/ComposeTransformSeries.cxx
+++ itk_source/ComposeTransformSeries.cxx
@@ -6,7 +6,7 @@
 #include "itkTransformFileReader.h"
 #include "itkTransformFileWriter.h"
 #include "itkTransformFactory.h"
-#include "itkMatrixOffsetTransformBase.h"
+#include "itkAffineTransform.h"
 
 #include "IOHelpers.hpp"
 #include "Dirs.hpp" 
@@ -16,7 +16,7 @@ void checkUsage(int argc, char const *argv[]) {
   if( argc < 3 )
   {
     cerr << "\nUsage: " << endl;
-    cerr << argv[0] << " firstInputDir secondInputDir outputDir\n\n";
+    cerr << argv[0] << " originalDir adjustmentDir outputDir\n\n";
     exit(EXIT_FAILURE);
   }
 }
@@ -26,10 +26,15 @@ int main(int argc, char const *argv[]) {
 	checkUsage(argc, argv);
 	
 	// Generate file lists
-  vector< string > firstInputFileNames  = constructPaths(argv[1], Dirs::ImageList(), ".meta");
-  vector< string > secondInputFileNames = constructPaths(argv[2], Dirs::ImageList(), ".meta");
-  vector< string > outputFileNames      = constructPaths(argv[3], Dirs::ImageList(), ".meta");
-
+  vector< string > basenames = directoryContents(argv[1]);
+  vector< string > originalPaths   = constructPaths(argv[1], basenames);
+  vector< string > adjustmentPaths = constructPaths(argv[2], basenames);
+  vector< string > outputPaths     = constructPaths(argv[3], basenames);
+  
+	// clear results directory
+  remove_all(argv[3]);
+  create_directory(argv[3]);
+	
 	// Some transforms might not be registered
   // with the factory so we add them manually
   itk::TransformFactoryBase::RegisterDefaultTransforms();
@@ -40,35 +45,46 @@ int main(int argc, char const *argv[]) {
 	typedef itk::TransformFileWriter WriterType;
 	// TranslationTransform also has a Compose() interface, but only with other TranslationTransforms
   typedef itk::MatrixOffsetTransformBase< double, 2, 2 > ComposableTransformType;
+  typedef itk::AffineTransform< double, 2 > AffineTransformType;
   
-  for(unsigned int i=0; i < firstInputFileNames.size(); ++i)
+  // the first and last slices should not have adjustments
+  // apply adjustments for every other slice
+  for(unsigned int i=1; i < originalPaths.size() - 1; ++i)
   {
     // Load input transforms
-    ReaderType::Pointer firstReader = ReaderType::New();
-    ReaderType::Pointer secondReader = ReaderType::New();
-    firstReader->SetFileName(  firstInputFileNames[i].c_str()  );
-    secondReader->SetFileName( secondInputFileNames[i].c_str() );
-    firstReader->Update();
-    secondReader->Update();
+    ReaderType::Pointer originalReader = ReaderType::New();
+    ReaderType::Pointer adjustmentReader = ReaderType::New();
+    originalReader->SetFileName(  originalPaths[i].c_str()  );
+    adjustmentReader->SetFileName( adjustmentPaths[i].c_str() );
+    originalReader->Update();
+    adjustmentReader->Update();
     
     // check that transforms are of the right dynamic type
-    ComposableTransformType *pFirstTransform  = dynamic_cast<ComposableTransformType*>( firstReader->GetTransformList()->begin()->GetPointer() );
-    ComposableTransformType *pSecondTransform = dynamic_cast<ComposableTransformType*>( secondReader->GetTransformList()->begin()->GetPointer() );
-    assert( pFirstTransform != 0 && pSecondTransform != 0 );
+    ComposableTransformType *pOriginalTransform  = dynamic_cast<ComposableTransformType*>( originalReader->GetTransformList()->begin()->GetPointer() );
+    ComposableTransformType *pAdjustmentTransform = dynamic_cast<ComposableTransformType*>( adjustmentReader->GetTransformList()->begin()->GetPointer() );
+    assert( pOriginalTransform != 0 && pAdjustmentTransform != 0 );
     
     // compose transforms
-    // If the argument pre is true, then other is precomposed with self; that is, the resulting transformation consists of first applying 
-    // other to the source, followed by self. If pre is false or omitted, then other is post-composed with self; that is the resulting
+    // If the argument pre is true (default false), then other is precomposed with self; that is, the resulting transformation consists of first
+    // applying other to the source, followed by self. If pre is false or omitted, then other is post-composed with self; that is the resulting
     // transformation consists of first applying self to the source, followed by other. This updates the Translation based on current center.
-    pFirstTransform->Compose(pSecondTransform);
+    AffineTransformType::Pointer outputTransform = AffineTransformType::New();
+    outputTransform->SetIdentity();
+    outputTransform->Compose(pOriginalTransform);
+    outputTransform->Compose(pAdjustmentTransform);
     
     // save output transform
     WriterType::Pointer writer = WriterType::New();
-    writer->SetFileName( outputFileNames[i].c_str() );
-    writer->AddTransform( pFirstTransform );
+    writer->SetFileName( outputPaths[i].c_str() );
+    writer->AddTransform( outputTransform );
     writer->Update();
     
   }
+  
+  // write out the unaltered first and last transforms
+  copy_file( *(originalPaths.begin()), *(outputPaths.begin()) );
+  copy_file( *(--originalPaths.end()), *(--outputPaths.end()) );
+  
   
   return EXIT_SUCCESS;
 }
