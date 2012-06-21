fd4105839ebc7f33748d9d39358d76e2f33fb57c
Matt Gibb <matt@mattgibb.com>
2012-06-21 22:35:17 +0100

    Simplified scaleImages

--- itk_source/lib/ImageFunctions/ScaleImages.hpp
+++ itk_source/lib/ImageFunctions/ScaleImages.hpp
@@ -1,25 +1,14 @@
 #ifndef SCALEIMAGES_HPP_
 #define SCALEIMAGES_HPP_
 
-#include "itkChangeInformationImageFilter.h"
-
 template <typename ImageType>
-void scaleImages(vector<typename ImageType::Pointer>& images, const typename ImageType::SpacingType& spacings) {
-  // rescale original images
-  typedef itk::ChangeInformationImageFilter< ImageType > ScalerType;
+void scaleImages(vector<typename ImageType::Pointer>& images, const typename ImageType::SpacingType& spacings)
+{
   typedef typename vector<typename ImageType::Pointer>::iterator IterType;
-  typename ScalerType::Pointer scaler;
-	for(IterType it = images.begin(); it != images.end(); ++it)
+  for(IterType it = images.begin(); it != images.end(); ++it)
 	{
-	  scaler = ScalerType::New();
-		scaler->ChangeSpacingOn();
-		scaler->SetOutputSpacing( spacings );
-    scaler->SetInput( *it );
-    scaler->Update();
-    *it = scaler->GetOutput();
-    (*it)->DisconnectPipeline();
+    (*it)->SetSpacing(spacings);
 	}
 }
-
 
 #endif
